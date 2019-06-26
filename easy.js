4f2adc7220c435a80e264d2e27159e7c19a92405
Brent Bumann <brent.bumann@aol.com>
2019-06-26 14:53:56 -0700

    Number converter works up to 10,000

--- easy.js
+++ easy.js
@@ -28,3 +28,91 @@ var maximumProduct = function (nums) {
   let ans2 = sortedNums[size - 1] * sortedNums[size - 2] * sortedNums[size - 3]
   return Math.max(ans1, ans2);
 };
+
+// Converting number into string
+
+const ONES = [
+ '',
+ 'one',
+ 'two',
+ 'three',
+ 'four',
+ 'five',
+ 'six',
+ 'seven',
+ 'eight',
+ 'nine'
+];
+
+const TEENS = [
+ 'ten',
+ 'eleven',
+ 'twelve',
+ 'thirteen',
+ 'fourteen',
+ 'fifteen',
+ 'sixteen',
+ 'seventeen',
+ 'eighteen',
+ 'nineteen'
+];
+
+const TENS = [
+ 'twenty',
+ 'thirty',
+ 'forty',
+ 'fifty',
+ 'sixty',
+ 'seventy',
+ 'eighty',
+ 'ninety'
+];
+
+const HUNDREDS = [
+ 'one-hundred',
+ 'two-hundred',
+ 'three-hundred',
+ 'four-hundred',
+ 'five-hundred',
+ 'six-hundred',
+ 'seven-hundred',
+ 'eight-hundred',
+ 'nine-hundred',
+];
+
+function convert(int){
+  if (int === 0) return;
+  if (int < 10) return ONES[int];
+  if (int < 20) return TEENS[int % 10];
+  
+  let divisor;
+  if (int > 1000){
+    divisor = 1000;
+  }else if (int > 100){
+    divisor = 100;
+  }else {
+    divisor = 10;
+  }
+
+  let remainder = int % divisor;
+  let number = Math.floor(int / divisor);
+  let stringified;
+
+  if (divisor == 10){
+    stringified = `${TENS[number - 2]} `;
+  }else if (divisor == 100){
+    stringified = ONES[number] + ' hundred ';
+  }else {
+    stringified = ONES[number] + ' thousand ';
+  }
+
+  return stringified + convert(remainder);
+}
+
+
+console.log(convert(115));
+console.log(convert(85));
+console.log(convert(123));
+console.log(convert(999));
+console.log(convert(1115));
+console.log(convert(9345));
