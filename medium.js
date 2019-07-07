1b76e7080976e77873412a1f88f093d76926af89
Brent Bumann <brent.bumann@aol.com>
2019-07-07 00:13:44 -0700

    Solve in faster time complexity

--- medium.js
+++ medium.js
@@ -34,3 +34,23 @@ var findMin = function (nums) {
 };
 
 // O(log(n)) solution
+// augmented binary search
+
+var findMin = function (nums) {
+  if (nums.length === 1) return nums[0];
+  let left = 0;
+  let right = nums.length - 1;
+
+  if (nums[left] < nums[right]) return nums[left];
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
+    if (nums[mid - 1] > nums[mid]) return nums[mid];
+    if (nums[mid] > nums[left]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+};
