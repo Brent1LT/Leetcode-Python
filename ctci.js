a353adbbab2a1198c4280d5fc7968283e46210e2
Brent Bumann <brent.bumann@aol.com>
2019-07-31 19:43:57 -0700

    Add check permutations

--- ctci.js
+++ ctci.js
@@ -11,3 +11,24 @@ function uniqueStr(string){
 
   return true;
 }
+
+function checkPermutation(string1, string2){
+  if(string1.length !== string2.length) return false;
+
+  let items = new Proxy({}, {
+    get: function(object, property){
+      return object.hasOwnProperty(property) ? object[property] : 0;
+    }
+  });
+  
+  for(let i = 0; i < string1.length; i++){
+    items[string1[i]] += 1;
+  }
+
+  for(let j = 0; j < string2.length; j++){
+    if(items[string2[j]] === 0) return false;
+    items[string2[j]] -= 1;
+  }
+
+  return true;
+}
